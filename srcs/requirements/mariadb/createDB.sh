dc32de13ebb9c55efb1e0e93f298a1bdf89e3b24
leonard <leonard.maurin@gmail.com>
2023-10-09 12:42:37 +0200

    homework

--- srcs/requirements/mariadb/createDB.sh
+++ srcs/requirements/mariadb/createDB.sh
@@ -1,5 +1,5 @@
 chmod 777 /run/mysqld/mysqld.sock
-service mysql start;
+service mysqld start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
