51c0ec1de70a12e22e9df052be7354561679871b
lmaurin- <leonard.maurin@gmail.com>
2023-10-09 19:26:04 +0200

    need to fix mariadb

--- srcs/requirements/mariadb/createDB.sh
+++ srcs/requirements/mariadb/createDB.sh
@@ -1,5 +1,4 @@
-chmod 777 /run/mysqld/mysqld.sock
-service mysqld start;
+service mysql start;
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
