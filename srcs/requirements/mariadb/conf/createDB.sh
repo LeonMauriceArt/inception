3c5d01ba9050052ebb908947ca79653af192a633
lmaurin <leonard.maurin@gmail.com>
2023-10-17 00:57:10 +0200

    fdsfdsf

--- srcs/requirements/mariadb/conf/createDB.sh
+++ srcs/requirements/mariadb/conf/createDB.sh
@@ -2,7 +2,7 @@ mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
 service mysql start;
 mysql -u root
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE DATABASE IF NOT EXISTS `${SQL_DATABASE}`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
