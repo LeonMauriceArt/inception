673e1a6912aecc82c48dc7e169ce9d51e96d88f8
lmaurin <leonard.maurin@gmail.com>
2023-10-20 17:50:04 +0200

    mariadb seems to be working

--- srcs/requirements/mariadb/conf/initDB.sh
+++ srcs/requirements/mariadb/conf/initDB.sh
@@ -4,19 +4,10 @@ echo "---Starting mysql---"
 service mysql start;
 echo "---Done!---"
 
-
 echo "---Configuring database and users---"
-echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE; \
-GRANT ALL ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD'; \
-FLUSH PRIVILEGES; \
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > db.sql
+echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> db.sql
+echo "FLUSH PRIVILEGES;" >> db.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" >> db.sql
+mysql < db.sql
 echo "---Done!---"
-
-
-#echo "---Shutdown---"
-service mysql -u root -p$SQL_ROOT_PASSWORD status
-service mysql -u root -p$SQL_ROOT_PASSWORD restart
-#echo "---Done!---"
-
-#echo "---Starting mysqld_safe---"
-mysqld_safe
