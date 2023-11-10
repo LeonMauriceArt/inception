477d3332805bd88502e58b0c93007993931823f3
lmaurin <leonard.maurin@gmail.com>
2023-11-10 19:57:14 +0100

    work

--- srcs/requirements/mariadb/conf/initDB.sh
+++ srcs/requirements/mariadb/conf/initDB.sh
@@ -9,11 +9,11 @@ mysql -u root -p$SQL_ROOT_PASSWORD << EOF
 CREATE DATABASE IF NOT EXISTS ${WORDPRESS_DB_NAME};
 CREATE USER '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${WORDPRESS_DB_NAME}.* TO '${SQL_USER}'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
 EOF
 echo "---Done!---"
 
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-exec mysqld_safe
+mysqld_safe --syslog
